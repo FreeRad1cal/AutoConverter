c264546b98fe8d9268b4f3cc1f097093d8d3e5f6
Boris K <bdk898@gmail.com>
2017-07-19 18:31:33 -0400

    Add project files.

---
+++ AutoConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
